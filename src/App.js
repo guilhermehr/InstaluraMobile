a8f977137f6489c9e67a2d25dd7ccb8cce50ac60
guilhermehr <guilhermehr@gmail.com>
2018-07-07 21:15:20 -0300

    integrando com API

--- src/App.js
+++ src/App.js
@@ -21,18 +21,25 @@ const instructions = Platform.select({
 });
 
 class InstaluraMobile extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fotos: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+      .then(resposta => resposta.json())
+      .then(json => this.setState({ fotos: json }));
+  }
+
   render() {
-
-    const fotos = [
-      { id: 1, usuario: 'rafael' },
-      { id: 2, usuario: 'alberto' },
-      { id: 3, usuario: 'vitor' }
-    ];
-
     return (
       <FlatList style={styles.container}
         keyExtractor={item => String(item.id)}
-        data={fotos}
+        data={this.state.fotos}
         renderItem={({ item }) =>
           <Post foto={item} />
         }
